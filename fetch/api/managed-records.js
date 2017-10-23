d7a8e7d43b54d8d2bb5706ef5eb2c689ec0bf45d
alexandraalday <alexandra.alday@gmail.com>
2017-10-22 19:04:46 -0700

    successful fetch of 10 result page

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -2,8 +2,63 @@ import fetch from "../util/fetch-fill";
 import URI from "urijs";
 
 // /records endpoint
-window.path = "http://localhost:3000/records";
+window.path = "http://localhost:3000/records?"; 
 
 // Your retrieve function plus any additional functions go here ...
+const retrieve = (options) => { 
+
+	let uri = new URI(window.path + 'limit=10&' + queryString);
+    let optionzzz = options
+    let queryString = [];
+
+    Object.toQueryString = (optionzzz, base) => {
+
+        Object.keys(optionzzz).forEach((key) => {
+
+            let result,
+                value;
+
+            value =  optionzzz[key];
+
+            if (base) {
+                key = base + '[]';
+            }
+            switch (typeof value) {
+                case 'object': 
+                    result = Object.toQueryString(value, key); 
+                    break;
+                case 'array':
+                    let qs = {};
+                    value.forEach((val, i) => {
+                        qs[i] = val;
+                    });
+                    result = Object.toQueryString(qs, key);
+                    break;
+                default: 
+                    result = 'offset=' + (value - 1);
+            }
+
+            if (value != null) {
+                queryString.push(result);
+            }
+        });
+        return queryString.join('&');
+    };
+
+
+    fetch(uri)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          return Promise.reject('Oh No! Something went wrong!')
+        }
+      })
+      .then(data => console.log('Have some data: ', data))
+      .catch(error => console.log('Peep this error: ', error));
+
+
+};
+
 
 export default retrieve;
