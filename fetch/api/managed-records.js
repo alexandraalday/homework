4d1c28f7c54056626648396c5ce9ba769750fdcc
alexandraalday <alexandra.alday@gmail.com>
2017-11-13 09:56:30 -0800

    transforms payload

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -9,30 +9,30 @@ const isPrimaryColor = (color) => {
   return ["red", "blue", "yellow"].includes(color);
 }
 
-const transform = (jsonPayload, page) => {
+const transform = (payload, page) => {
   console.log("Transforming!!")
-  console.log("jsonPayload: " + jsonPayload);
+  console.log("payload: " + payload);
   console.log("payload page: " + page);
   const previousPage = page && page > 1 ? page - 1 : null;
-  const nextPage = jsonPayload.length > 10 ? page + 1 : null;
+  const nextPage = payload.length > 10 ? page + 1 : null;
   console.log("previous page: " + previousPage);
   console.log("next page: " + nextPage);
 
-  if (jsonPayload.length > 10) {
-    jsonPayload.pop();
+  if (payload.length > 10) {
+    payload.pop();
   }
 
-  const ids = jsonPayload.map(item => item.id);
+  const ids = payload.map(item => item.id);
 
-  const open = jsonPayload
-    .filter(item => item.disposition === "open")
+  const open = payload
+    .filter(item => item.disposition == "open")
     .map(item => {
       item.isPrimary = isPrimaryColor(item.color);
       return item;
     });
 
-  const closedPrimaryCount = jsonPayload
-    .filter(item => item.disposition === "closed")
+  const closedPrimaryCount = payload
+    .filter(item => item.disposition == "closed")
     .reduce((total, item) => isPrimaryColor(item.color) ? total + 1 : total, 0);
 
   let result = { previousPage, nextPage, ids, open, closedPrimaryCount };
